c7262c9a952783884e0f58088d29ce324f359bc0
Thiago Ferreira Fernandes <thffmail@gmail.com>
2024-04-25 18:27:51 -0300

    Pacotes UI adicionados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  IgxButtonModule,
+  IgxIconModule,
+  IgxRippleModule,
+} from 'igniteui-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    IgxButtonModule,
+    IgxIconModule,
+    IgxRippleModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
